01a6bcdc8d38840a9f6208743511f85939e70ec2
unknown <eng.mohamedabdo98@gmail.com>
2021-10-07 06:34:18 +0200

    pushing the structure of the project

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const form = document.querySelector('#search-form');
 // Get the search field
 const searchField = document.querySelector('#search-keyword');
 // define avariable to put the search words in
-let searchedForText;
+let searchText;
 // Get the response container
 const responseContainer = document.querySelector('#response-container');
-
 
 
 
@@ -17,33 +16,18 @@ const responseContainer = document.querySelector('#response-container');
 // add listener to the form when submition
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    // put the search value into a variable 
-    searchedForText = searchField.value;
-
-    // make a new request 
-    let unsplashReq = new XMLHttpRequest();
-    // define the method and url of the request 
-    unsplashReq.open('Get', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-    // add the Authorization    
-    unsplashReq.setRequestHeader('Authorization', 'Client-ID 3iblNns8LGiwGcN2fqROuhumC_vsVUR5_jlXpjrIRBc');
-    // send the request to unsplash api
-    unsplashReq.send();
-    // Handle the sucsess request 
-    unsplashReq.onload = sucsessHandler;
-    // Handle the failed request 
-    unsplashReq.onerror = errorHandler;
+    // reset the content 
+    responseContainer.innerHTML= '';
     
-    // this funcition to Handle the sucsess request 
-    function sucsessHandler(){
-        const  data = JSON.parse(this.responseText);
+    // this funcition to Handle the sucsess of unsplash request 
+    const sucsessHandler=(data)=>{
         const firstImg = data.results[0];
         
-        if (data && data.results && data.results[0]){
+        if (firstImg){
             let htmlContent = `
             <figure>
-            <img src="${firstImg.urls.small}" alt="${searchedForText}" width="1000px">
-            <figcaption>taken by ${firstImg.user.name}</figcaption>
+                <img src="${firstImg.urls.small}" alt="${searchText}">
+                <figcaption>taken by ${firstImg.user.name}</figcaption>
             </figure>
             `
             // show the result on the screen 
@@ -54,27 +38,10 @@ form.addEventListener('submit', function (e) {
         }
     }
     
-    // this fuction to Handle the failed request
-    function errorHandler(err){
-        console.log('error :' , err);
-    }
-    
-    
-    
-    // make another request to news times 
-    const articleRequest = new XMLHttpRequest();
-    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9BqBn1TC1HEuFUEAqno5mf8GclmlHWgv`);
-    articleRequest.send();
-    articleRequest.onload = addArticles;
-    articleRequest.onerror = errorHandler;
-    
-    // this function will be trigger when the request sucsess
-    function addArticles () {
-        let data = JSON.parse(this.responseText);
-        console.log(data)
-
-        if (data.response && data.response.docs && data.response.docs.length>=1 ){
-            let articles = data.response.docs.map((article)=>`
+    // this funcition to Handle the sucsess of Time News request
+    const addArticles= (data)=> {
+        if (data.response.docs.length>1 ){
+            const articles = data.response.docs.map((article)=>`
             <li class="article">
                 <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
                 <p>${article.snippet}</p>
@@ -86,4 +53,32 @@ form.addEventListener('submit', function (e) {
             responseContainer.insertAdjacentHTML('beforeend' , `<div class="error-no-articles">No article available</div>`)
         }
     }
+    
+    // this fuction to Handle the failed request
+    const errorHandler= (err)=>{
+        console.log('error :' , err);
+    }
+
+
+    // put the search value into a variable 
+    searchText = searchField.value;
+    
+    // put the initializations of the request in variable
+    const intits = {
+        method  : 'GET',
+        headers : {'Authorization' : 'Client-ID b9ee_0RFMShrcjQHenznTYFYcCG9zsm_1-IJikJeKnQ'}
+    }
+
+    // fetch data from unsplash 
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchText}` , intits)
+    .then(response => response.json())
+    .then(sucsessHandler)
+    .catch(errorHandler)
+
+    // fech data from Time News
+    fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=48nqNs5BRQuCCJztzgbrMFWDykCLPpwi`)
+    .then(response=> response.json())
+    .then(addArticles)
+    .catch(errorHandler)
+
 });
